d66313c743ceff4fd15533195174209c610fb914
Mohammad <mreisi@yahoo.com>
2024-02-12 18:32:11 +0330

    Add hilt modules

---
+++ app/src/main/java/ir/cafebazaar/filmbazar/AppClass.kt
@@ -0,0 +1,7 @@
+package ir.cafebazaar.filmbazar
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppClass: Application()
---
+++ app/src/main/java/ir/cafebazaar/filmbazar/di/AppModule.kt
@@ -0,0 +1,10 @@
+package ir.cafebazaar.filmbazar.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+}
